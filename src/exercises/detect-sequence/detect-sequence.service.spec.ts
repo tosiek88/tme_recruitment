c3dbcf2df19516b7c354f581aff19c62d4e78ca6
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-05-30 17:28:40 +0200

    Init detect-sequence commit

--- src/exercises/detect-sequence/detect-sequence.service.spec.ts
+++ src/exercises/detect-sequence/detect-sequence.service.spec.ts
@@ -1,28 +1,25 @@
 import { expect } from "chai";
 import { DetectSequenceService } from "./detect-sequence.service";
 
-// describe("Sequence detection in array", () => {
-//   it("empty", ()=>{
-//     return true;
-//   });
-//   // it("No sequence found", () => {
-//   //   const inputSeq = [1, 2, 3];
-//   //   const searchedSeq = [8, 7];
+describe("Sequence detection in array", () => {
+  it("No sequence found", () => {
+    const inputSeq = [1, 2, 3];
+    const searchedSeq = [8, 7];
 
-//   //   expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(-1);
-//   // });
+    expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(-1);
+  });
 
-//   // it("One sequence in input sequence", () => {
-//   //   const inputSeq = [1, 2, 3, 8, 7, 1];
-//   //   const searchedSeq = [8, 7];
+  // it("One sequence in input sequence", () => {
+  //   const inputSeq = [1, 2, 3, 8, 7, 1];
+  //   const searchedSeq = [8, 7];
 
-//   //   expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(3);
-//   // });
+  //   expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(3);
+  // });
 
-//   // it("Two sequences in input we should return start index of second", () => {
-//   //   const inputSeq = [1, 22, 3, 8, 7, 8, 7, 1, 1, 1];
-//   //   const searchedSeq = [8, 7];
+  // it("Two sequences in input we should return start index of second", () => {
+  //   const inputSeq = [1, 22, 3, 8, 7, 8, 7, 1, 1, 1];
+  //   const searchedSeq = [8, 7];
 
-//   //   expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(5);
-//   // });
-// });
+  //   expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(5);
+  // });
+});
