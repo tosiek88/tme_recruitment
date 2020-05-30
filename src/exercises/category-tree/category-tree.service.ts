e7584ef5f236704ba38ceac3c9f599c1e4d7883e
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-05-30 17:27:45 +0200

    Renaming

--- src/exercises/category-tree/category-tree.service.ts
+++ src/exercises/category-tree/category-tree.service.ts
@@ -1,6 +1,6 @@
 import { FlatCategory, Category } from "./category.interface";
 
-const parseRecurency = (it: FlatCategory, arrayFlat: FlatCategory[]) => {
+const mapFlatTreeRecurency = (it: FlatCategory, arrayFlat: FlatCategory[]) => {
   const childs = arrayFlat.filter((child) => {
     if (it.id === child.parentId) {
       return child;
@@ -9,7 +9,7 @@ const parseRecurency = (it: FlatCategory, arrayFlat: FlatCategory[]) => {
 
   if (childs.length > 0) {
     (childs as Category[]).forEach((el, index) => {
-      childs[index] = parseRecurency(el, arrayFlat) as Category;
+      childs[index] = mapFlatTreeRecurency(el, arrayFlat) as Category;
     });
   }
 
@@ -22,7 +22,7 @@ export class CategoryTreeService {
    */
   public static buildNestedTree(flatTree: FlatCategory[]) {
     const categories = flatTree.reduce((result, it) => {
-      const category = parseRecurency(it, flatTree) as Category;
+      const category = mapFlatTreeRecurency(it, flatTree) as Category;
       if (!category.parentId) {
         result.push(category);
       }
