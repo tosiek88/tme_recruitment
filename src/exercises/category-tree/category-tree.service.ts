a09dd88f1f018f27ae404a5a1abd6897134442be
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-05-30 15:36:31 +0200

    Solution for tests

--- src/exercises/category-tree/category-tree.service.ts
+++ src/exercises/category-tree/category-tree.service.ts
@@ -1,13 +1,33 @@
-import { FlatCategory, Category } from './category.interface';
+import { FlatCategory, Category } from "./category.interface";
+
+const parseRecurency = (it: FlatCategory, arrayFlat: FlatCategory[]) => {
+  const childs = arrayFlat.filter((child) => {
+    if (it.id === child.parentId) {
+      return child;
+    }
+  });
+
+  if (childs.length > 0) {
+    (childs as Category[]).forEach((el, index) => {
+      childs[index] = parseRecurency(el, arrayFlat) as Category;
+    });
+  }
+
+  return { ...it, children: childs };
+};
 
 export class CategoryTreeService {
   /**
    * TODO Implement me!
    */
-  public static buildNestedTree(flatTree: FlatCategory[]): Category[] {
-    const category:Category[]=flatTree.map((it)=>{
-        return {...it, children:[]}
-      });
-    return category; 
+  public static buildNestedTree(flatTree: FlatCategory[]) {
+    const categories = flatTree.reduce((result, it) => {
+      const category = parseRecurency(it, flatTree) as Category;
+      if (!category.parentId) {
+        result.push(category);
+      }
+      return result;
+    }, [] as Category[]);
+    return categories;
   }
 }
