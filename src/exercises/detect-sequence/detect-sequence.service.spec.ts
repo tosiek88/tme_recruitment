0179d9fbaf21fa9397862fd4552851cac74764df
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-05-31 10:25:31 +0200

    Alternative solutions and tests

--- src/exercises/detect-sequence/detect-sequence.service.spec.ts
+++ src/exercises/detect-sequence/detect-sequence.service.spec.ts
@@ -16,9 +16,32 @@ describe("Sequence detection in array", () => {
   });
 
   it("Two sequences in input we should return start index of second", () => {
-    const inputSeq = [1, 22, 3, 8, 7, 8, 7, 1, 1, 1];
+    const inputSeq = [1, 22, 0.2, 8, 7, 8, 7, 1, 1, 1];
     const searchedSeq = [8, 7];
 
     expect(DetectSequenceService.detect(inputSeq, searchedSeq)).to.be.equal(5);
+  });
+});
+
+
+describe("Alternative Sequence detection in array", () => {
+  it("No sequence found", () => {
+    const inputSeq = [1, 2, 3];
+    const searchedSeq = [8, 7];
+    expect(DetectSequenceService.detect2(inputSeq, searchedSeq)).to.be.equal(-1);
+  });
+
+  it("One sequence in input sequence", () => {
+    const inputSeq = [1, 2, 3, 8, 7, 1];
+    const searchedSeq = [8, 7];
+
+   expect(DetectSequenceService.detect2(inputSeq, searchedSeq)).to.be.equal(3);
+  });
+
+  it("Two sequences in input we should return start index of second", () => {
+    const inputSeq = [1, 22, 0.2, 8, 7, 8, 7, 1, 1, 1];
+    const searchedSeq = [8, 7];
+
+    expect(DetectSequenceService.detect2(inputSeq, searchedSeq)).to.be.equal(5);
   });
 });
--- src/exercises/detect-sequence/detect-sequence.service.ts
+++ src/exercises/detect-sequence/detect-sequence.service.ts
@@ -1,3 +1,48 @@
+function buildPattern(seq: number[]) {
+  const patternTable = [0];
+  let prefixIndex = 0;
+  let suffixIndex = 1;
+
+  while (suffixIndex < seq.length) {
+    if (seq[prefixIndex] === seq[suffixIndex]) {
+      patternTable[suffixIndex] = prefixIndex + 1;
+      suffixIndex += 1;
+      prefixIndex += 1;
+    } else if (prefixIndex === 0) {
+      patternTable[suffixIndex] = 0;
+      suffixIndex += 1;
+    } else {
+      prefixIndex = patternTable[prefixIndex - 1];
+    }
+  }
+
+  return patternTable;
+}
+
+function knuthMorrisPratt(vector: number[], seq: number[]) {
+  let vectorIndex = 0;
+  let seqIndex = 0;
+
+  const patternTable = buildPattern(seq);
+
+  while (vectorIndex < vector.length) {
+    if (vector[vectorIndex] === seq[seqIndex]) {
+      // We've found a match.
+      if (seqIndex === seq.length - 1) {
+        return vectorIndex - seq.length + 1;
+      }
+      seqIndex += 1;
+      vectorIndex += 1;
+    } else if (seqIndex > 0) {
+      seqIndex = patternTable[seqIndex - 1];
+    } else {
+      seqIndex = 0;
+      vectorIndex += 1;
+    }
+  }
+
+  return -1;
+}
 
 export class DetectSequenceService {
   /**
@@ -5,10 +50,23 @@ export class DetectSequenceService {
    */
 
   public static detect(inputSeq: number[], searchedSeq: number[]): number {
-    const inputSeqString=inputSeq.map(it=>String.fromCharCode(it)).join('')
-    const searchSeqString=searchedSeq.map(it=>String.fromCharCode(it)).join('')
-    const result= inputSeqString.lastIndexOf(searchSeqString);
-    
+    const inputSeqString = inputSeq.map((it) => String.fromCharCode(it)).join("");
+    const searchSeqString = searchedSeq.map((it) => String.fromCharCode(it)).join("");
+    const result = inputSeqString.lastIndexOf(searchSeqString);
+    return result;
+  }
+
+  // Alternative solution
+  public static detect2(inputSeq: number[], searchedSeq: number[]): number {
+    let result = knuthMorrisPratt(inputSeq, searchedSeq);
+    if (result !== -1) {
+      // check if rest of array has pattern
+      do {
+        const arr = inputSeq.slice(result + searchedSeq.length, inputSeq.length + 1);
+        let index = knuthMorrisPratt(arr, searchedSeq);
+        result = index === -1 ? result : result + searchedSeq.length;
+      } while (result <= -1);
+    }
     return result;
   }
 }
