788e0b90e3c0678cf0e0a6735ce01addfe82ece1
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-06-01 11:07:25 +0200

    Merge table

--- src/exercises/rx-products-score/products-score.service.ts
+++ src/exercises/rx-products-score/products-score.service.ts
@@ -1,20 +1,41 @@
-import { Observable, of } from "rxjs";
-import {} from "rxjs/operators";
+import { Observable, from, of, merge, combineLatest, concat } from "rxjs";
+import { map, mergeMap, combineAll, take, toArray } from "rxjs/operators";
 import { ProductBasic, CustomerSymbol, DescriptionWords } from "./types";
 
 export class ProductsScoreService {
   /**
    * TODO Implement me!
    */
-  public static computeScore(
-    productsBasic$: Observable<ProductBasic>,
-    customerSymbols$: Observable<CustomerSymbol>,
-    descriptions$: Observable<DescriptionWords>,
-    inputPhrase: string,
-    productsLimit: number
-  ): Observable<string[]> {
-    const symbolsSortedByScoreDesc = of([]);
+  public static computeScore(productsBasic$: Observable<ProductBasic>, customerSymbols$: Observable<CustomerSymbol>, descriptions$: Observable<DescriptionWords>, inputPhrase: string, productsLimit: number): Observable<string[]> {
+    
 
+    const productsBasicArray=productsBasic$.pipe(
+      toArray(),
+    )
+    const customerSymbolsArray=customerSymbols$.pipe(
+      toArray(),
+    )
+    
+    const descriptionsArray=descriptions$.pipe(
+      toArray(),
+    )
+
+
+ const mergeById=<T extends {id:number},P extends {productId:number}>([t, s]:[T[], P[]]) => t.map(p => Object.assign({}, p, s.find(q => p.id === q.productId)));
+
+    const productsSymbols=combineLatest(productsBasicArray, customerSymbolsArray).pipe(
+      map(mergeById)
+    );
+
+
+    const all$=combineLatest(productsSymbols, descriptionsArray).pipe(
+      map(mergeById)
+    );
+
+
+
+    all$.subscribe(console.log);
+    const symbolsSortedByScoreDesc = of([]);
     return symbolsSortedByScoreDesc;
   }
 }
